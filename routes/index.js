faf294db13c9902552dc5b70c0e2166efa373c98
alburt <albert.asaftei00@gmail.com>
2024-05-18 12:29:08 +0200

    ✨ feat: current month expenses endpoint

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const router = Router();
 
 const prisma = new PrismaClient();
 
-/* GET home page. */
 router.get("/expenses", async (req, res) => {
   const expenses = await prisma.expenses.findMany({
     orderBy: {
@@ -13,6 +12,38 @@ router.get("/expenses", async (req, res) => {
   });
 
   res.json(expenses);
+});
+
+router.get("/expenses/currentMonth", async (req, res) => {
+  const currentMonthExpenses = await prisma.expenses.findMany({
+    where: {
+      createdAt: {
+        gte: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
+        lt: new Date(new Date().getFullYear(), new Date().getMonth() + 1, 1),
+      },
+    },
+  });
+
+  const expensesByCategory = currentMonthExpenses.reduce((acc, expense) => {
+    const existingCategory = acc.find(
+      (category) => category.categoryId === expense.categoryId
+    );
+
+    if (existingCategory) {
+      existingCategory.amount += expense.amount;
+    }
+
+    if (!existingCategory) {
+      acc.push({
+        categoryId: expense.categoryId,
+        amount: expense.amount,
+      });
+    }
+
+    return acc;
+  }, []);
+
+  res.json(expensesByCategory);
 });
 
 router.post("/expenses", async (req, res) => {
